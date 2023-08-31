4d257aa9ae5cf2ba77fa8b0c0b8ed4d9da290f6d
Napadensky <59300256+Napadensky@users.noreply.github.com>
2023-08-31 12:30:35 -0300

    fix: error parse JSON

--- src/infraestructure/http/routers/index.js
+++ src/infraestructure/http/routers/index.js
@@ -14,7 +14,7 @@ const httpStatus = require('http-status');
 const router = express.Router();
 
 const manageRouter = () => {
-  const routes = [router.get('/health', (_req, res) => {
+  const routes = [router.get('/api/v1/health', (_req, res) => {
     const filePath = path.join(__dirname, '../../../../demoUsers.json');
 
     fs.readFile(filePath, 'utf8', (err, data) => {
@@ -22,12 +22,24 @@ const manageRouter = () => {
         res.status(500).send('Error al leer el archivo');
         return;
       }
-      const convertJSON = JSON.parse(data)
-      console.log(convertJSON)
-      responseSuccess(res, { message: "OK", usersdemo: convertJSON }, httpStatus.OK)
 
+      // Verificar si data tiene contenido
+      if (!data || data.trim() === '') {
+        res.status(400).send('El archivo JSON está vacío');
+        return;
+      }
+
+      let convertJSON;
+      try {
+        convertJSON = JSON.parse(data);
+        console.log(convertJSON);
+        responseSuccess(res, { message: "OK", usersdemo: convertJSON }, httpStatus.OK);
+      } catch (error) {
+        console.error('Error al analizar el JSON:', error);
+        res.status(500).send('El contenido del archivo no es un JSON válido');
+      }
     });
-  })]
+  })];
 
   routes.push(router.use("/api/v1/auth", userRoute))
   routes.push(router.use("/api/v1/accounts", authMiddleware, accountRoute))
